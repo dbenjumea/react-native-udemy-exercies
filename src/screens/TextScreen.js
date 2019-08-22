9126cf818a5129616cf441f4cc27e5f9b7939c20
dbenjumea <d.benor@hotmail.com>
2019-08-22 22:02:14 +0100

    feat: added ternary expression to check if the password is longer than 3

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-    const [name, setName] = useState('');
+    const [password, setName] = useState('');
 
     return (
         <View>
-            <Text>Enter Name: </Text>
+            <Text>Enter Password: </Text>
             <TextInput
             style={styles.input}
             autoCapitalize="none"
             autoCorrect={false}
-            value={name} // The parent TextScreen gives the value to the child
+            value={password} // The parent TextScreen gives the value to the child
             onChangeText = {(newValue) => setName(newValue)} // but is the child who changes that value and the parent just listen
             />
-            <Text>My name is {name}</Text>
+            { password.length < 4 ? <Text>Password must be 4 characteres</Text> : null }  {/* // turnary expression */}
         </View>
     );
 };

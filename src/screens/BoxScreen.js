38d8af989ca9f0db898999001a935b610c16a73e
dbenjumea <d.benor@hotmail.com>
2019-08-28 14:59:38 +0100

    feat: using flex to allow children to get space required

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { View, Text, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Child #1</Text>
-            <Text style={styles.textStyle}>Child #2</Text>
-            <Text style={styles.textStyle}>Child #3</Text>
+            <Text style={styles.textOneStyle}>Child #1</Text>
+            <Text style={styles.textTwoStyle}>Child #2</Text>
+            <Text style={styles.textthreeStyle}>Child #3</Text>
         </View>
     );
 };
@@ -16,11 +16,19 @@ const styles = StyleSheet.create({
         borderWidth : 3,
         borderColor : 'black',
         //alignItems: 'center'
-        flexDirection: 'row', // column as well
+        //flexDirection: 'row', // column as well
         height: 200,
-        alignItems: 'center'
     },
-    textStyle: {
+    textOneStyle: {
+        borderWidth : 3, // works different in Android and iOS - iOS border increases to outside while in Android increases to inside, so in Android if you hve text aligned to left or right will get mixed with the border if it is thick, in Android has to be played with the padding, etc.
+        borderColor : 'red',
+    },
+    textTwoStyle: {
+        borderWidth : 3, // works different in Android and iOS - iOS border increases to outside while in Android increases to inside, so in Android if you hve text aligned to left or right will get mixed with the border if it is thick, in Android has to be played with the padding, etc.
+        borderColor : 'red',
+        //flex: 1 // 1 unit of space , if child 1 has 4, child 2 has 4 and child 3 has 2 = 4+4+2=10 units, so child 1 and 2 take 40% each and child 3 20%
+    },
+    textthreeStyle: {
         borderWidth : 3, // works different in Android and iOS - iOS border increases to outside while in Android increases to inside, so in Android if you hve text aligned to left or right will get mixed with the border if it is thick, in Android has to be played with the padding, etc.
         borderColor : 'red',
     }
